8daca898936c40d351378b93fced9dc631798ca1
yupo <yupo442@gmail.com>
2025-01-12 18:47:54 +0800

    Update ultrasonic_laser_gyro_copy_20250111174459.ino

--- PROTOTYPE 3.0(SD CARD & SIMPLE CALIBRATION)/ultrasonic_laser_gyro_copy_20250111174459.ino
+++ PROTOTYPE 3.0(SD CARD & SIMPLE CALIBRATION)/ultrasonic_laser_gyro_copy_20250111174459.ino
@@ -22,7 +22,6 @@ Adafruit_MPU6050 mpu;  // MPU6050 for accelerometer and gyroscope data
 unsigned long previousMillis = 0;
 const unsigned long interval = 1000;
 
-
 unsigned long ReadingPreviousMillis = 0;  // Counter for time indication
 const unsigned long ReadingInterval = 100;
 const unsigned long numReadings = interval / ReadingInterval;  // Number of readings in one second
@@ -46,31 +45,57 @@ double ultraReadings[numReadings];  // Array to store the readings
 String dataMessage;
 bool sdCardPresent = false;
 
-// By fair electro...... For more videos... https://www.youtube.com/channel/UCsn5KGbvt5Se7MaLsfNYthA
-
-#include <WiFi.h>
+#include <WiFi.h>  //By fair electro 24 Aug 2023 https://youtu.be/FNOC0GFGcG0?si=x4cHJ9zDt9cXv5z3
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 const long utcOffsetInSeconds = 8 * 3600;  // Adjust this based on your timezone.... 5 is for pakistan
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
-
-#include <Wire.h>
+#include <Wire.h>  //By fair electro 24 Aug 2023 https://youtu.be/FNOC0GFGcG0?si=x4cHJ9zDt9cXv5z3
 #include "RTClib.h"
 RTC_DS3231 rtc;
 
 #include <cmath>
 
-/*const char *ssid = "ipad";
-const char *password = "ily666777";*/
 
-const char *ssid = "Linksys02145";
-const char *password = "34pt8f7rxh";
+#define TX_PIN 17    // RS485 TX Pin
+#define RX_PIN 16    // RS485 RX Pin
+#define DE_RE_PIN 4  // RS485 Driver Enable/Receiver Enable Pin
+char tx_buffer[50];  // Buffer for data to send
+void RS485_SetTX() {
+  digitalWrite(DE_RE_PIN, HIGH);  // Enable Driver for Transmission
+}
+void RS485_SetRX() {
+  digitalWrite(DE_RE_PIN, LOW);  // Enable Receiver
+}
+void RS485_Send(const char *buf, uint16_t size) {
+  RS485_SetTX();                        // Switch to transmit mode
+  Serial2.write((uint8_t *)buf, size);  // Send data
+  Serial2.flush();                      // Wait until data is sent
+  RS485_SetRX();                        // Switch back to receive mode
+}
+
+/*const char *ssid = "";
+const char *password = "";*/
+
 
 void setup() {
   // Initialize serial communication
   Serial.begin(115200);
-  delay(1000);
+  WiFi.begin(ssid, password);
+  delay(3000);
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+  timeClient.begin();
+  timeClient.update();
+  if (WiFi.status() == WL_CONNECTED) {
+    rtc.adjust(DateTime(timeClient.getEpochTime()));
+  } else {
+    Serial.println("Couldn't find Wifi");
+  }
 
   // Initialize OLED display
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -105,20 +130,12 @@ void setup() {
   pinMode(triggerPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-  WiFi.begin(ssid, password);
-  delay(3000);
-  if (!rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
-    while (1) delay(10);
-  }
-  timeClient.begin();
-  timeClient.update();
-  if (WiFi.status() == WL_CONNECTED) {
-    rtc.adjust(DateTime(timeClient.getEpochTime()));
-  } else {
-    Serial.println("Couldn't find Wifi");
-  }
+  Serial2.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);  // Initialize RS485 Control Pin
+  pinMode(DE_RE_PIN, OUTPUT);
+  RS485_SetRX();  // Start in Receive mode
+
+  Serial.flush();
+  Serial2.flush();
 }
 
 void loop() {
@@ -129,8 +146,9 @@ void loop() {
   mpu.getEvent(&a, &g, &temp);
   float gyro = g.gyro.x;  // Get x-axis gyroscope data
 
-
   if (currentMillis - ReadingPreviousMillis >= ReadingInterval) {
+    Serial.flush();
+
     // --- VL53L0X Distance Measurement ---
     VL53L0X_RangingMeasurementData_t measure;
     lox.rangingTest(&measure, false);                                 // Perform measurement
@@ -153,13 +171,11 @@ void loop() {
 
     laserReadings[currentIndex] = laser;
     ultraReadings[currentIndex] = ultrasonicDistance;
-    Serial.print("currentIndex: ");
-    Serial.print(currentIndex);
-    Serial.printf(" %.0f    ", laser);
-    Serial.printf(" %.3f\n", ultrasonicDistance);
+
     currentIndex++;
     if (currentIndex >= numReadings) {
       if (currentMillis - previousMillis >= interval) {
+        Serial.flush();
         double UltraMaxValueInterval = ultraReadings[0];
         double UltraMinValueInterval = ultraReadings[0];
         double UltraRANGEinterval = 0;
@@ -170,6 +186,8 @@ void loop() {
         double LaserRANGEinterval = 0;
         laserSum = 0;
 
+        //by geeksforgeeks 28 Dec, 2024 https://www.geeksforgeeks.org/mathematics-mean-variance-and-standard-deviation/
+        //by Roel Van de Paar 23 Oct 2021 https://www.youtube.com/watch?v=NzWNJsG5EP8&t=38s&ab_channel=RoelVandePaar
         for (int i = 0; i < numReadings; i++) {
           ultrasonicDistanceSum += ultraReadings[i];
           if (ultraReadings[i] > UltraMaxValueInterval) {
@@ -211,7 +229,7 @@ void loop() {
         double LaserStdDev = sqrt(LaserSqDiff);
 
         // Print all sensor data to Serial Monitor
-        Serial.print(ultrasonicDistance, 4);
+        Serial.print(ultrasonicDistance, 4);  //to Python by Paul McWhorter 25 Jan 2022 https://www.youtube.com/watch?v=VN3HJm3spRE&ab_channel=PaulMcWhorter
         Serial.print(",");
         Serial.print(laser);
         Serial.print(",");
@@ -262,27 +280,19 @@ void loop() {
         display.display();
         display.clearDisplay();
 
-
-        Serial.printf("  Avg distance: %.4f", laser);
-        Serial.printf("  Prev: %.4f", previousLaser);
-        Serial.printf("  Difference: %.4f", diffLaserInOneSec);
-        Serial.printf("  R4NGEin1s: %.4f", LaserRANGEinterval);
-        Serial.printf("  var: %.4f", LaserSqDiff);
-        Serial.printf("  stdDev: %.4f\n", LaserStdDev);
-        Serial.printf("  Avg distance: %.4f", ultrasonicDistance);
-        Serial.printf("  Prev: %.4f", previousUltrasonic);
-        Serial.printf("  Difference: %.4f", diffUltraInOneSec);
-        Serial.printf("  R4NGEin1s: %.4f", LaserRANGEinterval);
-        Serial.printf("  var: %.4f", UltraSqDiff);
-        Serial.printf("  stdDev: %.4f\n\n", UltraStdDev);
+        snprintf(tx_buffer, sizeof(tx_buffer),
+                 "Ultrasonic: %.4f, Laser: %.4f, Diff Ultra: %.4f, Diff Laser: %.4f, Prev Ultra: %.4f, Prev Laser: %.4f, Gyro: %.2f",
+                 ultrasonicDistance, laser, diffUltraInOneSec, diffLaserInOneSec, previousUltrasonic, previousLaser, gyro);
+        RS485_Send(tx_buffer, strlen(tx_buffer));      // Transmit the data
+        Serial2.printf("Data sent: %s\n", tx_buffer);  // Print debug message
 
         dataMessage = padLeft(String(printLocalTime()), 19) + ", " + padLeft(String(ultrasonicDistance), 10) + ", " + padLeft(String(laser), 7) + ", " + padLeft(String(gyro), 7) + "\n";
         if (sdCardPresent) {
-          Serial.println("SD card is available!");
-          appendFile(SD, "/data.txt", dataMessage.c_str());
+          //Serial.println("SD card is available!");
+          appendFile(SD, "/data.txt", dataMessage.c_str());  //by Engr Fahad 21 Feb 2021 https://youtu.be/fPvW-dtB6i0?si=oah1L_oaQmai2ylV
           // Perform SD card operations here
         } else {
-          Serial.println("SD card is NOT available!");
+          //Serial.println("SD card is NOT available!");
         }
 
         checkSDCard();
@@ -299,28 +309,28 @@ void loop() {
 
 String printLocalTime() {
   DateTime now = rtc.now();
-  String timeString = String(now.year()) + "-" + (now.month() < 10 ? "0" : "") + String(now.month()) + "-" + (now.day() < 10 ? "0" : "") + String(now.day()) + " " + (now.hour() < 10 ? "0" : "") + String(now.hour()) + ":" + (now.minute() < 10 ? "0" : "") + String(now.minute()) + ":" + (now.second() < 10 ? "0" : "") + String(now.second());
-  return timeString;  // Print the formatted time
+  String timeString = String(now.year()) + "-" + (now.month() < 10 ? "0" : "") + String(now.month()) + "-" + (now.day() < 10 ? "0" : "") + String(now.day()) + " " + (now.hour() < 10 ? "0" : "") + String(now.hour()) + ":" + (now.minute() < 10 ? "0" : "") + String(now.minute()) + ":" + (now.second() < 10 ? "0" : "") + String(now.second());  //Chatgpt
+  return timeString;                                                                                                                                                                                                                                                                                                                                 // Print the formatted time
 }
 
+//ESP32 SD_Test Example https://github.com/espressif/arduino-esp32/blob/master/libraries/SD/examples/SD_Test/SD_Test.ino
 void appendFile(fs::FS &fs, const char *path, const char *message) {
   Serial.printf("Appending to file: %s\n", path);
   File file = fs.open(path, FILE_APPEND);
   if (!file) {
-    Serial.println("Failed to open file for appending");
+    //Serial.println("Failed to open file for appending");
     return;
   }
   if (file.print(message)) {
-    Serial.println("Message appended");
+    //Serial.println("Message appended");
   } else {
-    Serial.println("Append failed");
+    //Serial.println("Append failed");
   }
   file.close();
 }
 
 void checkSDCard() {
-  // If the SD card is already initialized, release it before reinitializing
-  SD.end();
+  SD.end();  //by Ralph S Bacon 15 Mar 2016 https://youtu.be/GQjtG1MeVs4?si=gjt1i7eOD1Qj9MW9
 
   // Try reinitializing the SD card
   if (!SD.begin()) {
@@ -334,11 +344,12 @@ void checkSDCard() {
     //Serial.println("No SD card attached.");
     sdCardPresent = false;
   } else {
-    Serial.println("SD card detected.");
+    //Serial.println("SD card detected.");
     sdCardPresent = true;
   }
 }
 
+//Chatgpt
 String padLeft(String str, int width) {
   while (str.length() < width) {
     str = " " + str;  // Add a space to the left until the desired width is reached
